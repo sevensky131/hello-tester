bd714622beff55884e7577df4afe9e859f2ad889
sevensky131 <duke131@163.com>
2019-04-06 00:10:16 +0800

    Puppy.java

---
+++ Puppy.java
@@ -0,0 +1,10 @@
+public class Puppy{
+   public Puppy(String name){
+      System.out.println("Puppy name is " + name ); 
+   }
+
+
+   public static void main(String[] args){
+      Puppy myPuppy = new Puppy( "tommy" );
+   }
+}
